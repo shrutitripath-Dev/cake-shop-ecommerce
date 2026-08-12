09b40d6b0d76d866554ecca79f424952da81d0e6
Saurabh Tripathi <tripathisaurabh332@gmail.com>
2026-08-13 03:31:58 +0530

    remove duplicate supabase client code

--- server/index.js
+++ server/index.js
@@ -5,6 +5,8 @@ import dotenv from "dotenv";
 import { createClient } from "@supabase/supabase-js";
 dotenv.config();
 
+
+
 const supabase = createClient(
   process.env.SUPABASE_URL,
   process.env.SUPABASE_KEY,
